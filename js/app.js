28ef968a475f6bcec0959d592138acb940b0c4a4
Adria Vieira <adriavieira731@gmail.com>
2019-05-24 19:16:07 -0300

    "Changed the size of the Play Again button"

--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@ $('#startGame').on('click', function(){
 
 function jogo(){
 	start(); //inicio do jogo
+	gameOver();
 	//funcao que adiciona um evento aos elementos do deck
 	$(".deck").on("click", function(evt){
 			if(evt.target.nodeName.toLowerCase() === 'li'){
